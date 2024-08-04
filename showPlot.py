658ed7c807fc84af45d29d3ec63cc83d6453fec7
readyshen <readyshen@gmail.com>
2024-08-04 23:10:56 +0800

    创建颜色和大小的映射

--- showPlot.py
+++ showPlot.py
@@ -7,7 +7,9 @@ def showDatingData():
     # 获取样本矩阵和类标签向量
     dDmat, dLabels = file2matirx('datingTestSet2.txt')
     fig, ax = plt.subplots()
-    ax.scatter(dDmat[:, 1], dDmat[:, 2])
+    # ax.scatter(dDmat[:, 1], dDmat[:, 2])
+    ax.scatter(dDmat[:, 1], dDmat[:, 2], c=array(dLabels), s=15.0 * array(dLabels))
+    # 使用标签向量作为颜色映射；点的大小与标签值成正比
     plt.show()
 
 
