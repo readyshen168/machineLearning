49f61f585f5b8fe45b3f23383f2594657bd74e13
readyshen <readyshen@gmail.com>
2024-08-08 21:26:32 +0800

    尝试不同的点大小参数

--- showPlot.py
+++ showPlot.py
@@ -10,7 +10,8 @@ def showDatingData():
     # ax.scatter(dDmat[:, 1], dDmat[:, 2])
     # 对dDmat做归一化处理
     dDmat = autoNorm(dDmat)[0]  # autoNorm返回的是由多个值组成的元组
-    ax.scatter(dDmat[:, 0], dDmat[:, 1], c=array(dLabels), s=15.0 * array(dLabels))
+    # ax.scatter(dDmat[:, 0], dDmat[:, 1], c=array(dLabels), s=15.0 * array(dLabels))
+    ax.scatter(dDmat[:, 0], dDmat[:, 1], c=array(dLabels), s=array(dLabels))
     # 使用标签向量作为颜色映射；点的大小与标签值成正比
     plt.show()
 
