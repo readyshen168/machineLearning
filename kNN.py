f34f085642b5bdbd3c9cffee2fbe607af5e2c8c5
readyshen <readyshen@gmail.com>
2024-08-10 22:54:44 +0800

    修改print方法

--- kNN.py
+++ kNN.py
@@ -13,6 +13,7 @@ def createDataSet():
     labels = ['A', 'A', 'B', 'B']
     return group, labels
 
+
 # k-邻近算法
 def classif0(inX, dataSet, labels, k):
     dataSetSize = dataSet.shape[0]
@@ -43,7 +44,7 @@ def file2matrix(filename):
         line = line.strip()
         listFromLine = line.split('\t')
         # 填充训练样本矩阵
-        returnMat[index,:] = listFromLine[0:3]
+        returnMat[index, :] = listFromLine[0:3]
         # 填充类标签向量
         classLabelVector.append(int(listFromLine[-1]))
         index += 1
@@ -62,8 +63,8 @@ def autoNorm(dataSet):
     # 得出数据集每个数与其所在列最小值差值
     normDataSet = dataSet - tile(minVals, (rows, 1))
     # 再除以每一列最大值与最小值之差
-    ranges = maxVals-minVals
-    normDataSet = normDataSet/tile(ranges, (rows, 1))
+    ranges = maxVals - minVals
+    normDataSet = normDataSet / tile(ranges, (rows, 1))
     # 返回归一化数据集， 原始数据每一列最大值与最小值的差，每一列的最小值
     return normDataSet, ranges, minVals
 
@@ -74,11 +75,11 @@ def datingClassTest():
     datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
     normMat, ranges, minVals = autoNorm(datingDataMat)
     m = normMat.shape[0]
-    numTestVecs = int(m*hoRtio)
+    numTestVecs = int(m * hoRtio)
     errorCount = 0.0
     for i in range(numTestVecs):
-        classifierResult = classif0(normMat[i,:], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
-        print "the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i])
-        if(classifierResult != datingLabels[i]): errorCount += 1.0
-    print "the total error rate is: %f" % (errorCount/float(numTestVecs))
-
+        classifierResult = classif0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        if classifierResult != datingLabels[i]:
+            errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount / float(numTestVecs)))
