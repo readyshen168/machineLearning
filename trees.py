371fd61c51815186d6d7e03363da616370c5b01c
readyshen <readyshen@gmail.com>
2024-08-27 23:08:27 +0800

    创建获取数据集的方法

--- trees.py
+++ trees.py
@@ -19,3 +19,15 @@ def calcShannonEnt(dataSet):
         shannoEnt -= prob * log(prob,2)
     # 返回熵值
     return shannoEnt
+
+
+# 获取数据集的方法
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
