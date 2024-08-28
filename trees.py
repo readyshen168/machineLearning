6507f06f56a38287f8e0e6437cc40ef4526c6d70
readyshen <readyshen@gmail.com>
2024-08-28 22:32:17 +0800

    按照给定特征划分数据集

--- trees.py
+++ trees.py
@@ -31,3 +31,13 @@ def createDataSet():
     labels = ['no surfacing', 'flippers']
     return dataSet, labels
 
+
+# 划分数据集的方法
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
